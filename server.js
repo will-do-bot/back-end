3103a0c299086a54925fb6f5c46126e13fe1dcbf
matheus1399 <matheusdeassiscaetano@gmail.com>
2018-02-20 20:34:46 -0300

    server updates

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 
 var express = require("express");
-var bodyParser = require("body-parser");
+var bodyParser = require("body-parser")
 
 
-var app = express();
-app.use(bodyParser.json());
+var app = express()
+app.use(bodyParser.json())
 
+app.listen(8080,function(){
+    console.log('Listening on PORT 8080')
+})
 
 app.get("/teste", function(req, res) {
-  res.send({result: 'This is the Task Bot'});
-});
+  res.send({result: 'This is the Task Bot'})
+})
