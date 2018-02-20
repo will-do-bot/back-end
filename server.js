2643b91dee8cc609a17eb1b225a0420a75dda8e3
matheus1399 <matheusdeassiscaetano@gmail.com>
2018-02-20 20:14:32 -0300

    first commit

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 
+var express = require("express");
+var bodyParser = require("body-parser");
 
-console.log('TaskBot')
+
+var app = express();
+app.use(bodyParser.json());
+
+
+app.get("/teste", function(req, res) {
+  res.send({result: 'This is the Task Bot'});
+});
