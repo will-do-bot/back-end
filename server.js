c25a9317fd06529a2fccc25ebcb85205993ac221
matheus1399 <matheusdeassiscaetano@gmail.com>
2018-02-21 08:40:31 -0300

    bug fix

--- server.js
+++ server.js
@@ -2,11 +2,10 @@
 var express = require("express");
 var bodyParser = require("body-parser")
 
-
 const mongoose = require('mongoose')
 require('dotenv').config()
 require('./models/Project')
-
+const User = mongoose.model('Project')
 mongoose.connect(process.env.DB_HOST)
 mongoose.connection.on('error', err => {
     console.log(err.message)
@@ -15,12 +14,18 @@ mongoose.connection.on('error', err => {
 var app = express()
 app.use(bodyParser.json())
 
-app.listen(process.env.PORT,function(){
-    console.log('Listening on PORT 8080')
+app.listen(process.env.PORT || 8080,function(){
+    console.log('Listening..')
 })
 
 app.use(express.static(__dirname + '/public'));
 
-app.get("/teste", function(req, res) {
-  res.send({result: 'This is the Task Bot'})
+app.post("/teste", function(req, res) {
+
+  const user =  (new User(req.body))
+  user.save((err, savedUser) => {
+      if (err)
+          return next({message: err.message, status: 400})
+      res.status(200).send(savedUser)
+  })
 })
