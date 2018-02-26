ddc351f86cb52fa94207c1e51ca40a2be9408413
matheus1399 <matheusdeassiscaetano@gmail.com>
2018-02-26 19:53:52 -0300

    front homepage

--- server.js
+++ server.js
@@ -6,17 +6,16 @@ const mongoose = require('mongoose')
 require('./models/Project')
 const User = mongoose.model('Project')
 
-mongoose.connect(process.env.MONGO_URI)
-mongoose.connection.on('error', err => {
-    console.log(err.message)
-})
+// mongoose.connect(process.env.MONGO_URI)
+// mongoose.connection.on('error', err => {
+//     console.log(err.message)
+// })
 
 var app = express()
 app.use(bodyParser.json())
 
 app.listen(process.env.PORT || 8080,function(){
     console.log('Listening..')
-    console.log(process.env.MONGODB_URI)
 })
 
 app.use(express.static(__dirname + '/public'));
