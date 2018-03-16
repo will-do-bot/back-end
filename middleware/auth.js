41f924763425a36ac6669c40870b3a5a09cfc91c
matheus1399 <matheusdeassiscaetano@gmail.com>
2018-03-15 22:12:22 -0300

    bug fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,14 +4,14 @@ const authController = require('./../controllers/auth')
 module.exports = {
 
     checkAuth: function (req, res, next) {
-        if (req.auth)
+        if (req.get('auth_key'))
             next();
         else
             res.status(400).send({ error: 'FORBIDDEN' })
     },
 
     validate: function (req, res, next) {
-        if (authController.getToke(req.auth))
+        if (authController.getToke(req.get('auth_key')))
             next();
         else
             res.status(400).send({ error: 'FORBIDDEN' })
