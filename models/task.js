40cbff45be1b75e118937cbfe46bb935a7f5b7e5
gmfonseca <32571975+gmfonseca@users.noreply.github.com>
2018-03-18 13:58:38 -0300

    Criação da tabela Task

---
+++ models/task.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+const projectSchema = new mongoose.Schema({
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    name: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    deadline: {
+        type: Date
+    },
+    startDate: {
+        type: Date
+    },
+    priority: {
+        type: String
+    },
+    project: {
+        type: Number
+    },
+    difficulty: {
+        type: Number
+    },
+    kind : {
+        type: String
+    },
+    timeTracker: {
+        type: Number
+    }
+})
+
+module.exports = mongoose.model('Task', projectSchema)
