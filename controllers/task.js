4edf03b8125a4c483acc79ef8e986b0f88ee3c64
gmfonseca <32571975+gmfonseca@users.noreply.github.com>
2018-03-19 17:22:45 -0300

    Update task.js : Troca do route pelo controller

--- controllers/task.js
+++ controllers/task.js
@@ -1,78 +1,24 @@
-const task_controller = require('./../controllers/task')
-const request = require('request')
-const auth = require('./../middleware/auth')
-const auth_controller = require('./../controllers/auth')
+const mongoose = require('mongoose');
+const Task = mongoose.model('Task');
 
-module.exports = function (app){
-
-    const PROJECT_NAME = "First" //arrumar referencia depois
-
-    app.post('/task',function(req, res){
-        if (req.query['name'] && PROJECT_NAME) {
-            let obj = {
-                'name': req.query['name'],
-                'description': req.query['description'],
-                'deadline': req.query['deadline'],
-                'startDate': req.query['start_date'],
-                'priority': req.query['priority'],
-                'project': req.query['project'],
-                'difficulty': req.query['difficulty'],
-                'kind': req.query['kind'],
-                'timeTracker': req.query['timeTracker']
-            };
-            controller.create(obj, (result, err) => {
-                if (err)
-                    res.status(500).send();
-                else {
-                    res.status(200).send(result);
-                }
-            })
-        }
-    })
-
-    app.get('/task/:id', auth.checkAuth, auth.validate, (req, res) => {
-        controller.getOne()
-    })
-    
-
-    app.get('/task', auth.checkAuth,  auth.validate,(req, res) => {
-        controller.list(PROJECT_NAME, function(err, obj) {
-            res.status(200).send(obj);
-        })
-    })
-
-    app.put('/task/:id', auth.checkAuth,  auth.validate,(req, res) => {
-        if (req.query['name'] && PROJECT_NAME) {
-            let obj = {
-                'name': req.query['name'],
-                'description': req.query['description'],
-                'deadline': req.query['deadline'],
-                'startDate': req.query['start_date'],
-                'priority': req.query['priority'],
-                'project': PROJECT_NAME,
-                'difficulty': req.query['difficulty'],
-                'kind': req.query['kind'],
-                'timeTracker': req.query['timeTracker']
-            };
-            controller.update(PROJECT_NAME, req.params.id, obj, (err, result) => {
-                if (err)
-                    res.status(500).send();
-                else {
-                    res.status(200).send(result);
-                }
-            })
-        }
-    })
-
-    app.delete('/task/:id', auth.checkAuth,  auth.validate,(req, res) => {
-        if (req.params.id && PROJECT_NAME) {
-            controller.remove(PROJECT_NAME, req.params.id, (err, result) => {
-                if (err)
-                    res.status(500).send();
-                else {
-                    res.status(200).send(result);
-                }
-            })
-        }
-    })
-}
+module.exports = {
+    create: function (obj, cb) {
+        let task = new Task(obj);
+        project.save(function (err, created) {
+            if (err) console.log(err);
+            cb(created);
+        });
+    },
+    list: function (project, cb) {
+        Task.find({ 'project': project }, cb);
+    },
+    getOne: function (project, id, cb) {
+        Task.find({ 'project': project, '_id': id }, cb);
+    },
+    update: function (project, id, newObj, cb) {
+        Task.update({ 'project': project, '_id': id }, newObj, cb);
+    },
+    remove: function (project, id, cb) {
+        Task.remove({ 'project': project, '_id': id }, cb);
+    }
+};
