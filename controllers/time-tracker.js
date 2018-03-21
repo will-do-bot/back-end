8a1ae182551c2c24f94c5952bea45dfed7b3233d
Lucas Silveira <lucas.silveira.2010@hotmail.com>
2018-03-21 10:00:17 -0300

    Alteração dos parâmetros do TimeTracker

--- controllers/time-tracker.js
+++ controllers/time-tracker.js
@@ -12,13 +12,13 @@ module.exports = {
     list: function (task, cb) {
         TimeTracker.find({ 'task': task }, cb);
     },
-    getOne: function (task, id, cb) {
-        TimeTracker.find({ 'task': task, '_id': id }, cb);
+    getOne: function (id, cb) {
+        TimeTracker.find({ '_id': id }, cb);
     },
-    update: function (task, id, newObj, cb) {
-        TimeTracker.update({ 'task': task, '_id': id }, newObj, { multi: true }, cb);
+    update: function (id, newObj, cb) {
+        TimeTracker.update({ '_id': id }, newObj, { multi: true }, cb);
     },
     remove: function (task, id, cb) {
-        TimeTracker.remove({ 'task': task, '_id': id }, cb);
+        TimeTracker.remove({ '_id': id }, cb);
     }
 };
