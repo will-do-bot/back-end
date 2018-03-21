b920814d1ff3e0cf894b7fd8790ad4f6337fa251
Brian Bruno <brian.bruno@outlook.com>
2018-03-21 19:04:07 -0300

    Gráfico na tela Chart

--- public/script.js
+++ public/script.js
@@ -1,5 +1,3 @@
-
-
 var app = angular.module('willdo', ["ngRoute"]);
 
 app.config(function ($routeProvider) {
@@ -101,7 +99,46 @@ app.controller('command', function ($scope) {
   console.log('command')
 })
 app.controller('chart', function ($scope) {
-  console.log('charts')
+  console.log('charts');
+    var ctx = document.getElementById("chartTasks");
+    Chart.defaults.global.legend.display = false;
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Antes do prazo", "No prazo", "Depois do prazo"],
+            datasets: [{
+                label: 'Time Tracking (Tarefas)',
+                data: [1, 18, 5],
+                backgroundColor: [
+                    '#f8bbd0',
+                    '#e91e63',
+                    '#c51162'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)'
+                ],
+                borderWidth: 0
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            },
+            legend: {
+                display: false
+            },
+            title: {
+                display: true,
+                text: 'Time Tracking - Conclusão das tarefas'
+            }
+        }
+    });
 })
 app.controller('calendar', function ($scope) {
   console.log('calendar')
