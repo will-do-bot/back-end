58046980cb247e79bf49e60fb0afcefe299573b9
matheus1399 <matheusdeassiscaetano@gmail.com>
2018-04-14 19:24:29 -0300

    delete inicial

--- public/controllers/project.js
+++ public/controllers/project.js
@@ -1,14 +1,18 @@
-app.controller('project', function ($scope, $httpController) {
+app.controller('project', function ($scope, $httpController, $http) {
 
-  $httpController.getProjects(projects => {
-    $scope.projects = projects;
-    $scope.chosenProject = projects[0];
-    $scope.getPriority = `priority ${$scope.chosenProject.priority.toLowerCase()}`
-    for (let i = 0; i < $scope.projects.length; i++) $scope.getTasks(i);
-    
-  })
 
-  $scope.getTasks = function (i){
+  getProjects()
+
+  function getProjects() {
+    $httpController.getProjects(projects => {
+      $scope.projects = projects;
+      $scope.chosenProject = projects[0];
+      $scope.getPriority = `priority ${$scope.chosenProject.priority.toLowerCase()}`
+      for (let i = 0; i < $scope.projects.length; i++) $scope.getTasks(i);
+    })
+  }
+
+  $scope.getTasks = function (i) {
     $httpController.getTasksOfProject($scope.projects[i]['_id'], tasks => {
       $scope.projects[i].tasks = tasks;
     })
@@ -22,6 +26,19 @@ app.controller('project', function ($scope, $httpController) {
   $scope.getChosen = function (x) {
     if ($scope.chosenProject.name == x.name) return 'task selected'
     else return 'task'
+  }
+
+  $scope.deleteProject = function (x) {
+    $http({
+      headers: {
+        auth_key: 'a5f0d15d-cd12-4d33-8212-7ddd9f2cb6b8'
+      },
+      method: 'DELETE',
+      url: '/project/' + x['_id']
+    })
+      .then(response => {
+        getProjects()
+      })
   }
 
 })
