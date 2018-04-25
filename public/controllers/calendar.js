eb87122b76609a8ebab2da9cd2044a3e12667703
Brian Bruno <brian.bruno@outlook.com>
2018-04-24 21:01:59 -0300

    Atualização na tela de Calendário

--- public/controllers/calendar.js
+++ public/controllers/calendar.js
@@ -1,6 +1,21 @@
 app.controller('calendar', function ($scope) {
     console.log('calendar');
 
+    $scope.pad = function (number, length) {
+        var str = '' + number;
+        while (str.length < length) {
+            str = '0' + str;
+        }
+        return str;
+    };
+
+    var today = new Date();
+    var dd = today.getDate();
+    var mm =  $scope.pad(today.getMonth()+1, 2);
+    var yyyy = today.getFullYear();
+
+    $scope.diaAtual = dd+"/"+mm.padStart(2, '0')+"/"+yyyy;
+
     $scope.tasks = {
         name : "Tasks",
         tasks : [
@@ -10,21 +25,23 @@ app.controller('calendar', function ($scope) {
                 priority: 'High'
             }
         ]
-    }
+    };
 
     $scope.chosenTask = $scope.tasks.tasks[0]
 
     $scope.changeContent = function (task) {
         $scope.chosenTask = task
-    }
+    };
 
     $scope.getPriority = function () {
         return "priority " + $scope.chosenTask.priority.toLowerCase()
-    }
+    };
 
     $scope.getChosen = function(x){
         if($scope.chosenTask.name == x.name) return 'task selected'
         else return 'task'
-    }
+    };
+
+
 
 });
