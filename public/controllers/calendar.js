8a48e9fcae06e4624c27fe4780ba1bc8fd15a1ff
Brian Bruno <brian.bruno@outlook.com>
2018-04-25 19:42:37 -0300

    Alterações na tela de calendário e chart

--- public/controllers/calendar.js
+++ public/controllers/calendar.js
@@ -1,4 +1,4 @@
-app.controller('calendar', function ($scope) {
+app.controller('calendar', function ($scope, $httpController) {
     console.log('calendar');
 
     $scope.pad = function (number, length) {
@@ -9,37 +9,36 @@ app.controller('calendar', function ($scope) {
         return str;
     };
 
-    var today = new Date();
-    var dd = today.getDate();
-    var mm =  $scope.pad(today.getMonth()+1, 2);
-    var yyyy = today.getFullYear();
+    let today = new Date();
+    let dd = today.getDate();
+    let mm =  $scope.pad(today.getMonth()+1, 2);
+    let yyyy = today.getFullYear();
+
+    $httpController.getTasks(tasks => {
+        $scope.tasks = tasks;
+        $scope.chosenTask = $scope.tasks[0];
+    });
 
     $scope.diaAtual = dd+"/"+mm.padStart(2, '0')+"/"+yyyy;
-
-    $scope.tasks = {
-        name : "Tasks",
-        tasks : [
-            {
-                name: 'Talk to the boss',
-                description: 'Talk to the boss about TIS',
-                priority: 'High'
-            }
-        ]
-    };
-
-    $scope.chosenTask = $scope.tasks.tasks[0]
 
     $scope.changeContent = function (task) {
         $scope.chosenTask = task
     };
 
     $scope.getPriority = function () {
-        return "priority " + $scope.chosenTask.priority.toLowerCase()
+        let resultado = "";
+        if($scope.chosenTask)
+            resultado = "priority " + $scope.chosenTask.priority.toLowerCase();
+        return resultado;
     };
 
     $scope.getChosen = function(x){
-        if($scope.chosenTask.name == x.name) return 'task selected'
+        if($scope.chosenTask.name === x.name) return 'task selected'
         else return 'task'
+    };
+
+    $scope.changeContent = function (task) {
+        $scope.chosenTask = task
     };
 
 });
