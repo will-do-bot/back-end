f860a9dc57b147fd94830158e55452b6ab8b5778
Brian Bruno <brian.bruno@outlook.com>
2018-04-25 21:01:59 -0300

    Alterações na tela de chart e calendar para recebimento dos dados do backend

--- public/controllers/calendar.js
+++ public/controllers/calendar.js
@@ -1,6 +1,12 @@
 app.controller('calendar', function ($scope, $httpController) {
     console.log('calendar');
 
+    $httpController.getTasks(tasks => {
+        $scope.tasks = tasks;
+        console.log(tasks);
+        $scope.chosenTask = $scope.tasks[0];
+    });
+
     $scope.pad = function (number, length) {
         var str = '' + number;
         while (str.length < length) {
@@ -14,31 +20,15 @@ app.controller('calendar', function ($scope, $httpController) {
     let mm =  $scope.pad(today.getMonth()+1, 2);
     let yyyy = today.getFullYear();
 
-    $httpController.getTasks(tasks => {
-        $scope.tasks = tasks;
-        $scope.chosenTask = $scope.tasks[0];
-    });
-
     $scope.diaAtual = dd+"/"+mm.padStart(2, '0')+"/"+yyyy;
 
     $scope.changeContent = function (task) {
         $scope.chosenTask = task
     };
 
-    $scope.getPriority = function () {
-        let resultado = "";
-        if($scope.chosenTask)
-            resultado = "priority " + $scope.chosenTask.priority.toLowerCase();
-        return resultado;
-    };
-
     $scope.getChosen = function(x){
-        if($scope.chosenTask.name === x.name) return 'task selected'
+        if($scope.chosenTask.name === x.name) return 'task selected';
         else return 'task'
-    };
-
-    $scope.changeContent = function (task) {
-        $scope.chosenTask = task
     };
 
 });
