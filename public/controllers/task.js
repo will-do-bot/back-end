adf51effa79117897df4e9bb2ec93e5a274332c9
matheus1399 <matheusdeassiscaetano@gmail.com>
2018-04-26 10:02:25 -0300

    a

--- public/controllers/task.js
+++ public/controllers/task.js
@@ -1,32 +1,37 @@
 app.controller('task', function ($scope, $httpController) {
 
-  $httpController.getTasks(tasks => {
-    console.log(tasks)
-    $scope.tasks = tasks
-    $scope.tasks.forEach(element => {
-      $httpController.getDays(element._id, resp => {
-        element.days = resp.diffDays
-      })
-    });
-    $scope.chosenTask = $scope.tasks[0]
-  })
 
-  $httpController.getProjects(projects =>{
+  getTask()
+  function getTask() {
+    $httpController.getTasks(tasks => {
+      console.log(tasks)
+      $scope.tasks = tasks
+      $scope.tasks.forEach(element => {
+        $httpController.getDays(element._id, resp => {
+          element.days = resp.diffDays
+        })
+      });
+      $scope.chosenTask = $scope.tasks[0]
+    })
+
+  }
+
+  $httpController.getProjects(projects => {
     $scope.projects = projects
   })
 
 
-  
+
 
   $scope.changeContent = function (task) {
     $scope.chosenTask = task
-    
+
   }
 
   $scope.getPriority = function () {
     return "priority " + $scope.chosenTask.priority.toLowerCase()
   }
-  
+
   $scope.showAddTask = false
 
   $scope.getChosen = function (x) {
@@ -36,37 +41,37 @@ app.controller('task', function ($scope, $httpController) {
 
   $scope.deleteTask = function (id) {
     console.log(id)
-    $httpController.deleteTask(id,response=>{
-      $httpController.getTasks(response=>{
-        console.log(response)
+    $httpController.deleteTask(id, response => {
+      $httpController.getTasks(response => {
+        getTask()
       })
     })
-    
+
   }
-  $scope.cancelTask = function(){
+  $scope.cancelTask = function () {
     $scope.showAddTask = !$scope.showAddTask
   }
-  $scope.addTask = function(task){
-    $httpController.setTask(task,response =>{
-      console.alert("Tarefa "+response.name +" criada com sucesso!");
+  $scope.addTask = function (task) {
+    $httpController.setTask(task, response => {
+      console.alert("Tarefa " + response.name + " criada com sucesso!");
     })
     $scope.showAddTask = !$scope.showAddTask
   }
 
-  $scope.startTask = function(id){
-   $httpController.startTask(id,response=>{
+  $scope.startTask = function (id) {
+    $httpController.startTask(id, response => {
       console.log(response)
     })
   }
-  $scope.pauseTask = function(id){
-    $httpController.pauseTask(id,response=>{
-       console.log(response)
-     })
-   }
-   $scope.finishTask = function(id){
-    $httpController.finishTask(id,response=>{
-       console.log(response)
-     })
-   }
+  $scope.pauseTask = function (id) {
+    $httpController.pauseTask(id, response => {
+      console.log(response)
+    })
+  }
+  $scope.finishTask = function (id) {
+    $httpController.finishTask(id, response => {
+      console.log(response)
+    })
+  }
 
 })
