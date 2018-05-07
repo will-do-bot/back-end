e9365e5789e85c168141e8d1cce413a0627fd8c8
Lucas Silveira <lucas.silveira.2010@hotmail.com>
2018-05-07 20:51:23 -0300

    registrar usuarios para receber notificações

--- controllers/reminder.js
+++ controllers/reminder.js
@@ -2,12 +2,12 @@ const schedule = require('node-schedule');
 const horario = { hour: 11, minute:00 };
 const taskController = require('./../controllers/task');
 
-module.exports = function() {
+module.exports = function(userID) {
     var j = schedule.scheduleJob(horario, function(){
         taskController.getByCond({}, (err, tasks) => {
             if (tasks[0] && !tasks[0]['finished']) {
                 let message = "Hello, user.. We suggest you to do now the task: " + tasks[0]['name'];
-                require('./../controllers/facebook').sendMessage("1636208479780756", message);
+                require('./../controllers/facebook').sendMessage(userID, message);
             }
         })
         
