7f3d19db5b349c33953014a396a9820d95309978
matheus1399 <matheusdeassiscaetano@gmail.com>
2018-05-16 10:02:05 -0300

    correcao das cagadas o lucas

--- decoders/project.js
+++ decoders/project.js
@@ -23,7 +23,7 @@ module.exports = {
                 });
                 break;
             case 'remove':
-                controllerProject.remove((result) => {
+                controllerProject.remove(obj2['id'],(result) => {
                     if (result.v == 1) {
                         cb(result.err, null)
                     } else {
